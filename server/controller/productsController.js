be758c46ca20681dac123c564320627678151bac
Tcodingg <princetlive@yahoo.com>
2022-08-20 15:36:55 -0400

    create produts conroller

---
+++ server/controller/productsController.js
@@ -0,0 +1,32 @@
+const Product = require('../models/Product');
+
+//Get all products
+const getAllProducts = async (req, res) => {
+	try {
+		const products = await Product.find({});
+		res.json(products);
+	} catch (err) {
+		console.log(error);
+		res.status(500).json({
+			message: error,
+		});
+	}
+};
+
+//Get single product
+const getProduct = async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+		res.json(product);
+	} catch (err) {
+		console.log(error);
+		res.status(500).json({
+			message: error,
+		});
+	}
+};
+
+module.exports = {
+	getAllProducts,
+	getProduct,
+};
