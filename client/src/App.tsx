6fcd1f5155cd798cc61ae2b4f9a55b1070e158cc
Tcodingg <princetlive@yahoo.com>
2022-08-21 18:18:40 -0400

    add all the pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,26 @@
 import Nav from './components/Nav/Nav';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+import Shop from './pages/Shop/Shop';
+import Cart from './pages/Cart/Cart';
+import SignUp from './pages/SignUp/SignUp';
+import Product from './pages/Product/Product';
 
 const App = () => {
 	return (
 		<BrowserRouter>
-			<div className='App'>
+			<>
 				<Nav />
 				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/product/:id' element={<Product />} />
+					<Route path='/shop' element={<Shop />} />
+					<Route path='/cart' element={<Cart />} />
 					<Route path='/login' element={<Login />} />
+					<Route path='/signup' element={<SignUp />} />
 				</Routes>
-			</div>
+			</>
 		</BrowserRouter>
 	);
 };
