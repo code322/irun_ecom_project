9f2923e7d97439dcd1e25db8d0b1fb2e9ebd3963
Tcodingg <princetlive@yahoo.com>
2022-08-22 12:20:46 -0400

    implement auth action types

---
+++ client/src/redux/actions/auth/authTypes.ts
@@ -0,0 +1,60 @@
+export enum actionTypes {
+	LOGIN_SUCCESS = 'LOGIN_SUCCESS',
+	LOGIN_FAIL = 'LOGIN_FAIL',
+	REGISTER_SUCCESS = 'REGISTER_SUCCESS',
+	REGISTER_FAIL = 'REGISTER_FAIL',
+	AUTH_FAIL = 'AUTH_FAIL',
+	USER_LOADING = 'USER_LOADING',
+	LOGOUT = 'LOGOUT',
+}
+
+export type userData = {
+	accessToken: string;
+	refreshToken: string;
+	user: {
+		name: string;
+		email: string;
+		id: string;
+	};
+};
+
+type actionUserLoading = {
+	type: actionTypes.USER_LOADING;
+};
+type actionLoginSuccess = {
+	type: actionTypes.LOGIN_SUCCESS;
+	payload: userData;
+};
+type actionRegisterSuccess = {
+	type: actionTypes.REGISTER_SUCCESS;
+	payload: userData;
+};
+
+type actionLoginFail = {
+	type: actionTypes.LOGIN_FAIL;
+	payload: any;
+};
+
+type actionRegisterFail = {
+	type: actionTypes.REGISTER_FAIL;
+	payload: any;
+};
+
+type actionAuthFail = {
+	type: actionTypes.AUTH_FAIL;
+	payload: any;
+};
+
+type actionLogout = {
+	type: actionTypes.LOGOUT;
+	payload: userData;
+};
+
+export type Actions =
+	| actionLoginSuccess
+	| actionLoginFail
+	| actionRegisterSuccess
+	| actionRegisterFail
+	| actionAuthFail
+	| actionUserLoading
+	| actionLogout;
