ec0a480bd6f5ac207aea08e1e2a5f6ae9b901716
Tcodingg <princetlive@yahoo.com>
2022-08-22 13:34:33 -0400

    combine all reducers

---
+++ client/src/redux/rootReducer.ts
@@ -0,0 +1,14 @@
+import { authReducer } from './reducers/auth/authReducer';
+import { cartReducer } from './reducers/product/cartReducer';
+import { productsReducer } from './reducers/product/productsReducer';
+import { productReducer } from './reducers/product/productReducer';
+import { combineReducers } from 'redux';
+
+export const rootReducer = combineReducers({
+	productsReducer,
+	productReducer,
+	cartReducer,
+	authReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
