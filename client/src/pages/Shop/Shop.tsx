cfe12ff9dc0c93bee8a2a17c8de3a9eec9dcb08f
Tcodingg <princetlive@yahoo.com>
2022-08-23 12:02:10 -0400

    remove unnecessary code

--- client/src/pages/Shop/Shop.tsx
+++ client/src/pages/Shop/Shop.tsx
@@ -1,14 +1,6 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import Products from '../../components/Products/Products';
-import { getAllProducts } from '../../redux/actions/product/actionsFetchProducts';
 import './Shop.scss';
 const Shop = () => {
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(getAllProducts());
-	}, [dispatch]);
-
 	return (
 		<section>
 			<div className='bd-container shop-container'>
