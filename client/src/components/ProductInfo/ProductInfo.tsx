e4650c9c34a6c522a62a1161dc8c05dd9d2573ec
Tcodingg <princetlive@yahoo.com>
2022-08-23 13:36:58 -0400

    create productinfo component

---
+++ client/src/components/ProductInfo/ProductInfo.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { BiMinus, BiPlus } from 'react-icons/bi';
+
+interface Props {
+	title: string;
+	description: string;
+}
+
+const ProductInfo: React.FC<Props> = ({ title, description }) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<div className='general-info-wrapper'>
+			<div
+				onClick={() => setIsOpen((prevState) => !prevState)}
+				className='general-info-content'
+			>
+				<p className='sub-title'>{title}</p>
+				<button>{isOpen ? <BiMinus /> : <BiPlus />} </button>
+			</div>
+			<p
+				style={{
+					maxHeight: isOpen ? '200px' : '0px',
+					overflow: 'hidden',
+					display: 'block',
+					transition: 'all 0.3s ease-out',
+				}}
+				className='text'
+			>
+				{description}
+			</p>
+		</div>
+	);
+};
+
+export default ProductInfo;
