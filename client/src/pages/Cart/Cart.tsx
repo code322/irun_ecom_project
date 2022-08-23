4145222157cb1ecd24a53300d616432e3c57b308
Tcodingg <princetlive@yahoo.com>
2022-08-23 14:59:09 -0400

    add cart items and sub totals

--- client/src/pages/Cart/Cart.tsx
+++ client/src/pages/Cart/Cart.tsx
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button/SqueryButton/Button';
+import { RootState } from '../../redux/rootReducer';
 import './Cart.scss';
-const Cart = () => {
-	return <div>Cart</div>;
+
+type checkoutTypes = {
+	subTotal: number;
+	taxes: number;
+	total: number;
+};
+const Cart: React.FC = () => {
+	const [checkout, setCheckout] = useState<checkoutTypes>({
+		subTotal: 0,
+		taxes: 0,
+		total: 0,
+	});
+
+	const nav = useNavigate();
+	const cart = useSelector((state: RootState) => state.cartReducer.cart);
+
+	const subTotal: number = cart.reduce(
+		(amount, item) => item.price * item.qty + amount,
+		0
+	);
+	const taxes: number = subTotal * 0.13;
+	const total: number = subTotal + taxes;
+	useEffect(() => {
+		setCheckout({
+			subTotal: subTotal,
+			taxes: Number(taxes.toFixed(2)),
+			total: total,
+		});
+	}, [subTotal, taxes, total]);
+
+	return (
+		<section className='cart'>
+			<div className='bd-container cart-container section'>
+				<div className='cart-items'>
+					{cart.map((item) => {
+						return (
+							<div key={item._id}>
+								<p>{item.title}</p>
+								<p>{item.details}</p>
+								<p>{item.price}</p>
+								<p>{item.images}</p>
+								<p>{item.qty}</p>
+								<p>{item.inStock}</p>
+								<p>{item._id}</p>
+							</div>
+						);
+					})}
+				</div>
+				<div className='cart-checkout'>
+					<div className='cart-checkout-container'>
+						<p className='title'>summary</p>
+						<div className='cart-checkout-wrapper'>
+							<div className='checkout-left'>
+								<p className='text'>subtotal</p>
+								<p className='text'>taxes</p>
+							</div>
+							<div className='checkout-right'>
+								<p className='text'>{`$${checkout.subTotal.toFixed(2)}`}</p>
+								<p className='text'>{`$${checkout.taxes.toFixed(2)}`}</p>
+							</div>
+						</div>
+						<div className='checkout-bottom'>
+							<p className='text'>total</p>
+							<p className='text'>{`$${checkout.total.toFixed(2)}`}</p>
+						</div>
+					</div>
+					<div className='checkout-btn-container'>
+						<Button text='Check Out' handleClick={() => nav('/checkout')} />
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Cart;
