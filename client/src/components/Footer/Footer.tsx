46ff858c2ad582b59aba3a5274a82d4947e43608
Tcodingg <princetlive@yahoo.com>
2022-08-24 10:14:49 -0400

    create a footer

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,60 @@
+import {
+	RiFacebookFill,
+	RiInstagramLine,
+	RiPinterestFill,
+	RiTwitterLine,
+} from 'react-icons/ri';
+import './Footer.scss';
+
+const Footer: React.FC = () => {
+	return (
+		<footer>
+			<div className='footer-container bd-container '>
+				<div className='footer-top grid'>
+					<div>
+						<h4 className='title'>about Store</h4>
+						<p className='text'>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Distinctio eius quis aute
+						</p>
+					</div>
+					<div>
+						<h4 className='title'>customer service</h4>
+						<ul>
+							<li>contact us</li>
+							<li>return and refund</li>
+							<li>terms and conditions</li>
+							<li>online store</li>
+						</ul>
+					</div>
+					<div>
+						<h4 className='title'>My Profile</h4>
+						<ul>
+							<li>my account</li>
+							<li>checkout</li>
+							<li>help </li>
+							<li>support</li>
+						</ul>
+					</div>
+					<div className='footer-icons'>
+						<h4 className='title'>connect with us</h4>
+						<div>
+							<RiFacebookFill />
+
+							<RiInstagramLine />
+							<RiTwitterLine />
+							<RiPinterestFill />
+						</div>
+					</div>
+				</div>
+				<div className='footer-bottom'>
+					<small>
+						&#169; {new Date().getFullYear} iRun. All rights reserved.
+					</small>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
