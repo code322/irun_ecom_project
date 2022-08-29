915865a3ba848fa66e931aa5ddebec74075385f4
Tcodingg <princetlive@yahoo.com>
2022-08-29 19:40:37 -0400

    validate users input as users type in the register page

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import Button from '../../components/Button/SqueryButton/Button';
 import { register } from '../../redux/actions/auth/authActions';
 import './Register.scss';
+import { validEmail as valid } from '../../utils/helpers';
+
+type inputType = {
+	name: string;
+	email: string;
+	password: string;
+	confirmPassword: string;
+};
 const Register: React.FC = () => {
-	const [input, setInput] = useState({
+	const [input, setInput] = useState<inputType>({
 		name: '',
 		email: '',
 		password: '',
 		confirmPassword: '',
 	});
 
+	const [isValidName, setIsValidName] = useState<boolean | null>(null);
+	const [isValidEmail, setIsValidEmail] = useState<boolean | null>(null);
+	const [isValidPassword, setIsValidPassword] = useState<boolean | null>(null);
+	const [isValidConfirmPassword, setIsValidConfirmPassword] = useState<
+		boolean | null
+	>(null);
 	const dispatch = useDispatch();
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,6 +34,41 @@ const Register: React.FC = () => {
 			[name]: value,
 		});
 	};
+
+	let validEmail = valid(input.email);
+
+	useEffect(() => {
+		function validateInput() {
+			// validate name
+			input.name.length === 0
+				? setIsValidName(null)
+				: input.name.length > 4
+				? setIsValidName(true)
+				: setIsValidName(false);
+
+			// validate password
+			input.password.length === 0
+				? setIsValidPassword(null)
+				: input.password.length < 6
+				? setIsValidPassword(false)
+				: setIsValidPassword(true);
+
+			// validate confirm password
+			input.confirmPassword.length === 0
+				? setIsValidConfirmPassword(null)
+				: input.password === input.confirmPassword
+				? setIsValidConfirmPassword(true)
+				: setIsValidConfirmPassword(false);
+			// validate email
+			input.email.length === 0
+				? setIsValidEmail(null)
+				: validEmail
+				? setIsValidEmail(true)
+				: setIsValidEmail(false);
+		}
+		validateInput();
+	}, [input.name, input.email, input.confirmPassword, input.password]);
+
 	return (
 		<section className='register'>
 			<div className='bd-container register-container section flex'>
