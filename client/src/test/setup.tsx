f9dcdd4e621131eea1cd85b3055ec31827e51cb2
Tcodingg <princetlive@yahoo.com>
2022-09-13 18:12:30 -0400

    setup test

---
+++ client/src/test/setup.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode, ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { localStore } from '../redux/store';
+
+interface Props {
+	children?: ReactNode;
+	// any props that come into the component
+}
+const RootWrapper: React.FC = ({ children }: Props) => {
+	return (
+		<BrowserRouter>
+			<Provider store={localStore}>{children}</Provider>
+		</BrowserRouter>
+	);
+};
+
+// const customRender = (ui: any, options: any) =>
+// 	render(ui, { wrapper: RootWrapper, ...options });
+
+const customRender = (
+	ui: ReactElement,
+	options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: RootWrapper, ...options });
+// re-export everything
+export * from '@testing-library/react';
+
+export { customRender as render };
