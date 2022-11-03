aab895aea8a0a3fc857dd169ff9157d0e6af5e48
Tcodingg <princetlive@yahoo.com>
2022-11-03 11:37:07 -0400

    fix formatting

--- client/src/pages/Login/Login.test.tsx
+++ client/src/pages/Login/Login.test.tsx
@@ -3,39 +3,40 @@ import '@testing-library/jest-dom/extend-expect';
 import Login from './Login';
 
 describe('Login page', () => {
-	describe('layout', () => {
-		render(<Login />);
-		it('has a header', () => {
-			const header = screen.getByRole('heading', {
-				name: 'already registered?',
-			});
-			expect(header).toBeInTheDocument();
-		});
-		it('has email input field', () => {
-			render(<Login />);
-			const email = screen.getByPlaceholderText('Email *');
-			expect(email).toBeInTheDocument();
-		});
+  describe('layout', () => {
+    render(<Login />);
+    it('has a header', () => {
+      const header = screen.getByRole('heading', {
+        name: 'already registered?',
+      });
+      expect(header).toBeInTheDocument();
+    });
+    it('has email input field', () => {
+      render(<Login />);
+      const email = screen.getByPlaceholderText('Email *');
+      expect(email).toBeInTheDocument();
+    });
 
-		it('has password input field', () => {
-			render(<Login />);
-			const password = screen.getByPlaceholderText('Password *');
-			expect(password).toBeInTheDocument();
-		});
-		it('should have password type for the password input field', () => {
-			render(<Login />);
-			const password = screen.getByPlaceholderText('Password *');
-			expect(password.type).toBe('password');
-		});
+    it('has password input field', () => {
+      render(<Login />);
+      const password = screen.getByPlaceholderText('Password *');
+      expect(password).toBeInTheDocument();
+    });
+    it('should have password type for the password input field', () => {
+      render(<Login />);
+      const password =
+        screen.getByPlaceholderText<HTMLInputElement>('Password *');
+      expect(password.type).toBe('password');
+    });
 
-		it('has a Sign In an account button', () => {
-			render(<Login />);
-			const button = screen.getByRole('button', {
-				name: /sign in/i,
-			});
-			expect(button).toBeInTheDocument();
-		});
-	});
+    it('has a Sign In an account button', () => {
+      render(<Login />);
+      const button = screen.getByRole('button', {
+        name: /sign in/i,
+      });
+      expect(button).toBeInTheDocument();
+    });
+  });
 
-	describe('Interactions', () => {});
+  describe('Interactions', () => {});
 });
