a7bfe9f75908e0aa8601576657c003275248e091
Tcodingg <princetlive@yahoo.com>
2022-11-04 11:07:48 -0400

    change the items tag to list

--- client/src/components/Products/Products.tsx
+++ client/src/components/Products/Products.tsx
@@ -6,32 +6,31 @@ import { RootState } from '../../redux/rootReducer';
 import './Products.scss';
 
 const Products: React.FC = () => {
-	const dispatch = useDispatch();
-	const { products } = useSelector((state: RootState) => state.productsReducer);
-	console.log(products);
-	return (
-		<section className='product-list'>
-			{products.map(({ title, price, gender, _id, images }) => {
-				return (
-					<div className='product' key={_id}>
-						<Link
-							onClick={() => dispatch(getProduct(_id))}
-							to={`/product/${_id}`}
-						>
-							<img src={images[0]} alt='' />
-							<div className='product-details'>
-								<div className='product-info'>
-									<p className='text'>{title}</p>
-									<p className='gender'>{`${gender}'s Shoes`}</p>
-								</div>
-								<p className='text price'>${price}</p>
-							</div>
-						</Link>
-					</div>
-				);
-			})}
-		</section>
-	);
+  const dispatch = useDispatch();
+  const { products } = useSelector((state: RootState) => state.productsReducer);
+  return (
+    <ul className='product-list'>
+      {products.map(({ title, price, gender, _id, images }) => {
+        return (
+          <li className='product' key={_id}>
+            <Link
+              onClick={() => dispatch(getProduct(_id))}
+              to={`/product/${_id}`}
+            >
+              <img src={images[0]} alt='' />
+              <div className='product-details'>
+                <div className='product-info'>
+                  <p className='text'>{title}</p>
+                  <p className='gender'>{`${gender}'s Shoes`}</p>
+                </div>
+                <p className='text price'>${price}</p>
+              </div>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Products;
