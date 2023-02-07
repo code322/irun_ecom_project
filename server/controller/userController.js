ad04e3c83ba9dbbf85d2bb4443f26f0d40342c53
Tcodingg <princetlive@yahoo.com>
2023-02-07 15:28:01 -0500

    format userController.js

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -4,115 +4,115 @@ const User = require('../models/userModule');
 
 // login user
 const login = async (req, res) => {
-	const { email, password } = req.body;
+  const { email, password } = req.body;
 
-	try {
-		// check if  the user exists
-		const user = await User.findOne({ email: email });
-		if (!user) return res.status(400).json('user does not exists');
+  try {
+    // check if  the user exists
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json('user does not exists');
 
-		// validate the password
-		const validPassword = await bcrypt.compare(password, user.password);
-		if (!validPassword)
-			return res.status(400).json({ message: 'invalid password' });
-		const accessToken = jwt.sign({ _id: user._id }, process.env.ACCESS_TOKEN, {
-			expiresIn: 30,
-		});
-		const refreshToken = jwt.sign(
-			{ _id: user._id },
-			process.env.REFRESH_TOKEN,
-			{
-				expiresIn: '30d',
-			}
-		);
-		if (!accessToken) return res.json('no token');
-		if (!refreshToken) return res.json('no refreshToken');
+    // validate the password
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res.status(400).json({ message: 'invalid password' });
+    const accessToken = jwt.sign({ _id: user._id }, process.env.ACCESS_TOKEN, {
+      expiresIn: 30,
+    });
+    const refreshToken = jwt.sign(
+      { _id: user._id },
+      process.env.REFRESH_TOKEN,
+      {
+        expiresIn: '30d',
+      }
+    );
+    if (!accessToken) return res.json('no token');
+    if (!refreshToken) return res.json('no refreshToken');
 
-		res.json({
-			accessToken,
-			refreshToken,
-			user: {
-				email: user.email,
-				name: user.name,
-				id: user._id,
-			},
-		});
-	} catch (error) {
-		console.log(error);
-	}
+    res.json({
+      accessToken,
+      refreshToken,
+      user: {
+        email: user.email,
+        name: user.name,
+        id: user._id,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 // register user
 const register = async (req, res) => {
-	const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-	try {
-		// check if the users user
-		const exists = await User.findOne({ email: email });
+  try {
+    // check if the users user
+    const exists = await User.findOne({ email: email });
 
-		if (exists) return res.status(400).json('Email is already registered');
+    if (exists) return res.status(400).json('Email is already registered');
 
-		// if the users doesnt exist
-		const newUser = new User({ name, email, password });
-		bcrypt.genSalt(10, (err, salt) => {
-			bcrypt.hash(newUser.password, salt, async (err, hash) => {
-				if (err) throw err;
-				newUser.password = hash;
+    // if the users doesnt exist
+    const newUser = new User({ name, email, password });
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, async (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
 
-				const user = await newUser.save();
+        const user = await newUser.save();
 
-				const accessToken = jwt.sign(
-					{ _id: user._id },
-					process.env.ACCESS_TOKEN,
-					{ expiresIn: 10 }
-				);
-				if (!accessToken) return res.json('no access token');
+        const accessToken = jwt.sign(
+          { _id: user._id },
+          process.env.ACCESS_TOKEN,
+          { expiresIn: 10 }
+        );
+        if (!accessToken) return res.json('no access token');
 
-				const refreshToken = jwt.sign(
-					{ _id: user._id },
-					process.env.REFRESH_TOKEN,
-					{ expiresIn: 30 }
-				);
-				if (!refreshToken) return res.json('no access token');
-				res.json({
-					accessToken,
-					refreshToken,
-					user: {
-						id: user.id,
-						name: user.name,
-						email: user.email,
-					},
-				});
-			});
-		});
-	} catch (error) {
-		console.log(error);
-	}
+        const refreshToken = jwt.sign(
+          { _id: user._id },
+          process.env.REFRESH_TOKEN,
+          { expiresIn: 30 }
+        );
+        if (!refreshToken) return res.json('no access token');
+        res.json({
+          accessToken,
+          refreshToken,
+          user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          },
+        });
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // refresh token
 const refreshToken = async (req, res) => {
-	const refToken = req.body.refreshToken;
+  const refToken = req.body.refreshToken;
 
-	if (!refToken)
-		return res
-			.status(401)
-			.json({ message: 'no refresh token. please login again' });
+  if (!refToken)
+    return res
+      .status(401)
+      .json({ message: 'no refresh token. please login again' });
 
-	try {
-		// console.log(refToken);
-		const decoded = jwt.verify(refToken, process.env.REFRESH_TOKEN);
-		req.user = decoded;
-		const id = req.user._id;
-		const accessToken = jwt.sign({ _id: id }, process.env.ACCESS_TOKEN, {
-			expiresIn: 30,
-		});
-		await res.json({
-			accessToken: accessToken,
-		});
-	} catch (error) {
-		res.status(400).json({ message: 'token can not be verified' });
-	}
+  try {
+    // console.log(refToken);
+    const decoded = jwt.verify(refToken, process.env.REFRESH_TOKEN);
+    req.user = decoded;
+    const id = req.user._id;
+    const accessToken = jwt.sign({ _id: id }, process.env.ACCESS_TOKEN, {
+      expiresIn: 30,
+    });
+    await res.json({
+      accessToken: accessToken,
+    });
+  } catch (error) {
+    res.status(400).json({ message: 'token can not be verified' });
+  }
 };
 
 module.exports = { login, register, refreshToken };
