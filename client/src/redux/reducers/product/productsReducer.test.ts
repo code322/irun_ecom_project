c651d2c9e5e7c043d1071f59e8f0b1ea768c9b97
Tcodingg <princetlive@yahoo.com>
2023-02-16 09:20:32 -0500

    test: mock the redux store and test if the data is being fetched

--- client/src/redux/reducers/product/productsReducer.test.ts
+++ client/src/redux/reducers/product/productsReducer.test.ts
@@ -7,14 +7,14 @@ import { getAllProducts } from '../../actions/product/actionsFetchProducts';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 import { product as responseBodyType } from '../../actions/product/actionTypes';
-import * as reactRedux from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 import { productsData } from '../../../test/dummyData';
-import { RootState } from '../../rootReducer';
 
 let responseData: responseBodyType[] = productsData;
 let url = 'http://localhost:5000';
-
+const mockStore = configureStore([thunk]);
 const server = setupServer(
   rest.get<responseBodyType>(`${url}/api/products`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(responseData));
@@ -27,7 +27,7 @@ beforeEach(() => {
 beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
 afterAll(() => server.close());
 
-describe('products reducer', () => {
+describe('products reducer with a mock dispatch function', () => {
   it('should return the initial state when passed an empty action', () => {
     const initialState = undefined;
     const action = { type: '' };
@@ -71,5 +71,18 @@ describe('products reducer', () => {
     expect(calls).toHaveLength(2);
     expect(calls[0][0].type).toEqual('GET_ALL_PRODUCTS_LOADING');
     expect(calls[1][0].type).toEqual('GET_ALL_PRODUCTS_FAIL');
+  });
+});
+
+describe('products store with mocked redux store', () => {
+  it('should fetch the products data', async () => {
+    const store = mockStore({ loading: false, products: productsData });
+    await store.dispatch(getAllProducts() as any);
+
+    const actions = store.getActions();
+
+    expect(actions).toHaveLength(2);
+    expect(actions[0].type).toEqual('GET_ALL_PRODUCTS_LOADING');
+    expect(actions[1].type).toEqual('GET_ALL_PRODUCTS_SUCCESS');
   });
 });
