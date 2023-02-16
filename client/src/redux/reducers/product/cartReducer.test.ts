df5a2bf7b54973cfdedb339dd0d4162788ff99b0
Tcodingg <princetlive@yahoo.com>
2023-02-16 16:58:14 -0500

    test: it should add product into the cart reducer

--- client/src/redux/reducers/product/cartReducer.test.ts
+++ client/src/redux/reducers/product/cartReducer.test.ts
@@ -1,4 +1,6 @@
-import { cartReducer } from './cartReducer';
+import { productsData } from './../../../test/dummyData';
+import { actionTypes } from '../../actions/product/actionTypes';
+import { cartReducer, initialState } from './cartReducer';
 
 describe('cart reducer', () => {
   it('should return the initial state when empty action is passed', () => {
@@ -7,5 +9,14 @@ describe('cart reducer', () => {
     const result = cartReducer(initialState, action as any);
 
     expect(result).toMatchObject({ cart: [] });
+  });
+  it('should add product into the cart', () => {
+    let state = initialState;
+    let action = { type: actionTypes.ADD_TO_CART, payload: productsData[1] };
+    let result = cartReducer(state, action as any);
+    state = {
+      cart: [{ ...productsData[1], qty: 1 }],
+    };
+    expect(result).toEqual(state);
   });
 });
