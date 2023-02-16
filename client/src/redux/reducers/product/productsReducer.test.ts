803a94857cffa4adc4b6b7ad88b7911efc23c6bf
Tcodingg <princetlive@yahoo.com>
2023-02-16 08:51:46 -0500

    test: it should error message if fetching fails

--- client/src/redux/reducers/product/productsReducer.test.ts
+++ client/src/redux/reducers/product/productsReducer.test.ts
@@ -1,10 +1,16 @@
-import { productsReducer, initialState } from './productsReducer';
+import {
+  productsReducer,
+  initialState,
+  interfaceState,
+} from './productsReducer';
 import { getAllProducts } from '../../actions/product/actionsFetchProducts';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 import { product as responseBodyType } from '../../actions/product/actionTypes';
+import * as reactRedux from 'react-redux';
 
 import { productsData } from '../../../test/dummyData';
+import { RootState } from '../../rootReducer';
 
 let responseData: responseBodyType[] = productsData;
 let url = 'http://localhost:5000';
@@ -32,7 +38,30 @@ describe('products reducer', () => {
 
   it('should fetch the products data', async () => {
     const dispatch = jest.fn();
-    const state = initialState;
+    const state: interfaceState = { loading: false, products: productsData };
+    const thunk = getAllProducts();
+
+    await thunk(dispatch, () => state, undefined);
+
+    const { calls } = dispatch.mock;
+
+    // console.log(state);
+
+    expect(calls).toHaveLength(2);
+    expect(calls[0][0].type).toEqual('GET_ALL_PRODUCTS_LOADING');
+    expect(calls[1][0].type).toEqual('GET_ALL_PRODUCTS_SUCCESS');
+    expect(calls[1][0].payload).toEqual(productsData);
+    // console.log(dispatch.mock.calls[1][0].type);
+  });
+  it('should return err message if fetching fails', async () => {
+    let error: 'no products found';
+    server.use(
+      rest.get(`${url}/api/products`, (req, res, ctx) => {
+        return res(ctx.status(400), ctx.json({ message: error }));
+      })
+    );
+    const dispatch = jest.fn();
+
     const thunk = getAllProducts();
 
     await thunk(dispatch);
@@ -41,8 +70,6 @@ describe('products reducer', () => {
 
     expect(calls).toHaveLength(2);
     expect(calls[0][0].type).toEqual('GET_ALL_PRODUCTS_LOADING');
-    expect(calls[1][0].type).toEqual('GET_ALL_PRODUCTS_SUCCESS');
-    expect(calls[1][0].payload).toEqual(productsData);
-    // console.log(dispatch.mock.calls[1][0].type);
+    expect(calls[1][0].type).toEqual('GET_ALL_PRODUCTS_FAIL');
   });
 });
