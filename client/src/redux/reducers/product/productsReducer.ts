a0ae6f4b650bb18fea09c1dd210e14f8d8a87789
Tcodingg <princetlive@yahoo.com>
2023-02-22 09:10:35 -0500

    format the products reducer

--- client/src/redux/reducers/product/productsReducer.ts
+++ client/src/redux/reducers/product/productsReducer.ts
@@ -1,40 +1,44 @@
+import { RootState } from './../../rootReducer';
 import { Actions, actionTypes } from '../../actions/product/actionTypes';
 import { product } from '../../actions/product/actionTypes';
 
-interface interfaceState {
-	loading: boolean;
-	products: product[];
-	err?: String;
+export interface interfaceState {
+  loading: boolean;
+  products: product[];
+  err?: String;
 }
 
-let initialState: interfaceState = {
-	loading: false,
-	products: [],
+export let initialState: interfaceState = {
+  loading: false,
+  products: [],
 };
 
 export const productsReducer = (
-	state: interfaceState = initialState,
-	action: Actions
+  state: interfaceState = initialState,
+  action: Actions
 ): interfaceState => {
-	switch (action.type) {
-		case actionTypes.REQUEST_ALL_PRODUCTS_LOADING:
-			return {
-				...state,
-				loading: true,
-			};
-		case actionTypes.REQUEST_ALL_PRODUCTS_SUCCUSS:
-			return {
-				...state,
-				loading: false,
-				products: action.payload,
-			};
-		case actionTypes.REQUEST_ALL_PRODUCTS_FAIL:
-			return {
-				...state,
-				loading: false,
-				err: action.payload,
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case actionTypes.REQUEST_ALL_PRODUCTS_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case actionTypes.REQUEST_ALL_PRODUCTS_SUCCUSS:
+      return {
+        ...state,
+        loading: false,
+        products: action.payload,
+      };
+    case actionTypes.REQUEST_ALL_PRODUCTS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        err: action.payload,
+      };
+    default:
+      return state;
+  }
 };
+
+export const selectProducts = (state: RootState) =>
+  state.productsReducer.products;
