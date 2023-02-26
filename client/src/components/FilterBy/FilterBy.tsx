c9583982732a51abdf0da924ccc000fa3675104b
Tcodingg <princetlive@yahoo.com>
2023-02-26 12:00:53 -0500

    install material ui dependecy

--- client/src/components/FilterBy/FilterBy.tsx
+++ client/src/components/FilterBy/FilterBy.tsx
@@ -1,7 +1,9 @@
-import { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import './FilterBy.scss';
 import { priceRangeType } from '../../pages/Shop/Shop';
 import { FiSearch } from 'react-icons/fi';
+import Slider from '@mui/material/Slider';
+
 interface Props {
   search: string;
   setSearch: React.Dispatch<React.SetStateAction<string>>;
@@ -51,6 +53,30 @@ const FilterBy: FC<Props> = (props: Props) => {
     }
   }, [rangeValue]);
 
+  //----
+  function valuetext(value: number) {
+    console.log(value);
+    return `${value}°C`;
+  }
+  const minDistance = 10;
+  const [value1, setValue1] = React.useState<number[]>([20, 37]);
+
+  const handleChange1 = (
+    event: Event,
+    newValue: number | number[],
+    activeThumb: number
+  ) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+    } else {
+      setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+    }
+  };
+
   return (
     <div className='filter-by-container'>
       <input onChange={handleSearch} type='text' placeholder='Search...' />
@@ -88,6 +114,14 @@ const FilterBy: FC<Props> = (props: Props) => {
           <option value='male'>Male</option>
           <option value='female'>Female</option>
         </select>
+        <Slider
+          getAriaLabel={() => 'Minimum distance'}
+          value={value1}
+          onChange={handleChange1}
+          valueLabelDisplay='auto'
+          getAriaValueText={valuetext}
+          disableSwap
+        />
       </div>
     </div>
   );
